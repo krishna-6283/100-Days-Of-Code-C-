d75d5f9132aef0dea0a5fc95b77ae53bb799d7bc
Gopal Krishna Shrivastava <122596081+krishna-6283@users.noreply.github.com>
2024-04-12 21:18:57 +0530

    Add files via upload

---
+++ day 15/usingLInkedList.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// using namespace std;
+
+// class ListNode{
+// 	public:
+// 	int val;
+// 	ListNode* next;
+// 	ListNode(int x){
+// 		val=x;
+// 		next=NULL;
+// 	}
+// };
+
+// class queue{
+// 	public:
+// 	ListNode* top;
+// 	ListNode* back;
+// 	int size;
+// 	int capacity;
+// 	queue(int c){
+// 		capacity=c;
+// 		top=NULL;
+// 		size=0;
+// 	}
+// };
+
+// void push(int d){
+// 	if(size==0){
+// 		top=new ListNode(data);
+// 		back=top;
+// 		size++;
+// 		return ;
+// 	}
+// 	if(size==capacity){
+// 		cout<<"Queue overflow"<<endl;
+// 		return ;
+// 	}
+// }
+
+// int main(){
+
+// }
